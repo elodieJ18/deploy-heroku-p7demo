bd281ccb28e434dc8ca85d3c74749a209587fa94
Elodie Jean <elodie.jean18@gmail.com>
2022-07-19 00:59:34 +0200

    react-hook-data-connexion-failed-but token ok

--- front/src/App.js
+++ front/src/App.js
@@ -1,37 +1,36 @@
 import "./App.css";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Route from './components/Route';
+import { UidContext } from "./components/AppContext";
 import axios from "axios";
-import { token } from "morgan";
-import { EmailJSResponseStatus } from "@emailjs/browser";
 
-function App() {
+const App = () => {
+    const [uid, setUid] = useState(null);
 
-      axios({
-        method: "get",
-        url: `${process.env.REACT_APP_API_URL}jwtid`,
-        withCredentials: true,
-      })  .then(function (response) {
-        console.log(response.data);
-        return response.data;
-    })
-      .then((res) => {
-        if (res.error) { 
-          console.log(res.error);
-          alert("The response data is invalid")
-        } })
-        .catch((err) => {
-          console.log("No token")
-          let errorMessage = typeof err.response !== "undefined" ? err.response.data.message : err.message;
-          alert(errorMessage);
-        });
+    useEffect(() => {
+      const fetchToken = async () => {
+        await axios({
+          method: "get",
+          url: `${process.env.REACT_APP_API_URL}jwtid`,
+          withCredentials: true,
+      })  .then((res) => {
+        setUid(res.data)
+        console.log(res.data)
+        console.log("console.log (res.data)")
+        console.log(res)
+        console.log("console.log (res)")
+      })
+      .catch((err) => console.log("No token"));
+    };
+      fetchToken();
+    }, [uid]);
+    
+    console.log(uid)
  
-
-
   return (
-    <div> 
+    <UidContext.Provider value={uid}> 
       <Route/>
-    </div> 
+    </UidContext.Provider> 
   );
 }
 
