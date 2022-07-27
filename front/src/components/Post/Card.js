c23c619c4bfa4feb3ddef84368641de4e105f351
Elodie Jean <elodie.jean18@gmail.com>
2022-07-27 22:44:05 +0200

    creation home comparaison id

--- front/src/components/Post/Card.js
+++ front/src/components/Post/Card.js
@@ -1,41 +1,52 @@
 import React, { useState, useEffect } from "react";
 import { useSelector,} from "react-redux";
 import "../../css/styles.css";
-import profil from "../../assets/user-200.png";
-import { isEmpty } from '../Utils';
+import logo from "../../assets/user-200.png";
+
 
 export const Card = ({comment}) => {
-    const usersData = useSelector((state) => state.userSReducer);
+    const usersData = useSelector((state) => state.usersReducer);
     const userData = useSelector((state) => state.userReducer);
+   
 
   return (
     <div className="home-card-container" key={comment.id}>
     
       <div className="home-card-userProfil">
-        <img /*src={!isEmpty(usersData[0] && usersData.map((user) => {
-            if (usersData.id === comment.id){
+        <img src={(usersData?.[0] && usersData.map((user) => {
+            if (comment.id === user.id){
+                console.log("test")
+                console.log(user.id)
                  return user.image;
-            } else return null;
+            } else return logo;
             })
             .joint("")
-            )} alt="profil-pic"*/ />
+            )} alt="profil-pic" />
         <div className="home-card-userStatus">
         <div className="home-card-usersStatus-second-col">
-          <p className="home-card-description-name">Name</p>
-          <p className="home-card-description-status">Status</p> 
+          <p className="home-card-description-name">{(usersData?.[0] && usersData.map((user) => {
+            if (user.id === comment.id){
+                 return user.nom;
+            } else return logo;
+            })
+            .joint("")
+            )}</p>
+          <p className="home-card-description-status">{(usersData?.[0] && usersData.map((user) => {
+            if (user.id === comment.id){
+                console.log("test")
+                 return user.status;
+            } else return logo;
+            })
+            .joint("")
+            )}</p> 
         </div>
         <div className="home-card-usersStatus-second-col"> 
-          <p className="home-card-description-date">Date</p>
+          <p className="home-card-description-date">{comment.date}</p>
           </div> 
         </div>
       </div>
       <div className="home-card-description">
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis 
-              nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-              Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore 
-              eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
-              sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <p>{comment.message}</p>
       </div>
       <div className="home-card-reaction">
           icons
