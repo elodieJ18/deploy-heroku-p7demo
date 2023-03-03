86c0e8288d9042171d391c793ceefc6d449796f0
lody <lody@mbp-de-lody.home>
2023-03-03 15:15:12 -0800

    dadabase

--- back/routes/likes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const LikesCtrl = require("../controllers/likes");
-const { authJwt } = require("../middleware");
-//likes
-router.post("/", [authJwt.verifyToken], LikesCtrl.likeComment);
-router.get("/",[authJwt.verifyToken], LikesCtrl.getallLike);
-
-module.exports = router;
