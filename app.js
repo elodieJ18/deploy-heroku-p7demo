2783f0f85f5be0ebf490b6fa90dce93c18f5845e
lody <lody@mbp-de-lody.home>
2023-03-06 13:25:10 -0800

    cors

--- app.js
+++ app.js
@@ -25,6 +25,21 @@ app.use(express.json());
 
 const cors = require('cors');
 
+const whitelist = ['http://localhost:3000', 'https://groupomaniademo.herokuapp.com/']
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin)
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable")
+      callback(null, true)
+    } else {
+      console.log("Origin rejected")
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(cors(corsOptions))
+
 
   app.use(
               cors({
