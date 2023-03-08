d0ed4d6fea677b855006c3302f204b6f8b5ce65a
lody <lody@mbp-de-lody.home>
2023-03-08 11:47:46 -0800

    correction-cors-post/put image

--- app.js
+++ app.js
@@ -25,20 +25,6 @@ app.use(express.json());
 
 const cors = require('cors');
 
-const whitelist = ['https://groupomaniademo.herokuapp.com/']
-const corsOptions = {
-  origin: function (origin, callback) {
-    console.log("** Origin of request " + origin)
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      console.log("Origin acceptable")
-      callback(null, true)
-    } else {
-      console.log("Origin rejected")
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-app.use((corsOptions))
 
 
   app.use(
