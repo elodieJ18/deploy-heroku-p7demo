0c944c5dfe3cc568211482b15f2fcb5ce673d173
lody <lody@mbp-de-lody.home>
2023-03-08 18:30:42 -0800

    test-image

--- controllers/comment.js
+++ controllers/comment.js
@@ -6,33 +6,8 @@ const { image } = require("../middleware/cloudinary");
 //créer post comment
 module.exports.createComment  = async (req, res) => {
   try {
-    if (!req.body.message) {
-      return res.status(400).send({
-        message: "Content can not be empty!"
-      });
-    }
-    console.log(req.body);
-    console.log(req.file);  
-   
-    let { comment } = ({
-      id: req.body,
-      idObject: req.body, 
-      message: req.body,
-      date: req.body,
-      image: image.secure_url,
-      cloudinary_id: image.public_id,
-      });
-  
-    if (req.file) {
-       image = await cloudinary.uploader.upload(req.file.path);
-       return image.public_id;
-      }
-      else {
-        image = null;
-    }
-    Comment.create({
-      comment
-    }).then((comment) => res.status(201).send(comment))
+   const result = await cloudinary.uploader.upload(req.file.path);
+   res.json(result);
   } catch (error) {
     console.log(error);
     return res.send(`Error: ${error}`);
