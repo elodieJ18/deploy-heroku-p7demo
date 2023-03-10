525cad57276b1385b7168573cde597fd079466ca
lody <lody@mbp-de-lody.home>
2023-03-10 00:02:11 -0800

    path

--- routes/reply.js
+++ routes/reply.js
@@ -5,13 +5,13 @@ const { authJwt } = require("../middleware");
 const upload = require("../middleware/multer");
 
 //creation d'une nouvelle sauce
-router.post("/upload", [authJwt.verifyToken], upload.single('image'), replyCtrl.createReply);
+router.post("/upload", [authJwt.verifyToken], upload, replyCtrl.createReply);
 //afficher toutes les sauces
 router.get("/",[authJwt.verifyToken], replyCtrl.getallReply);
 //affichage du produit dans sa propre page
 router.get("/:idObject",[authJwt.verifyToken], replyCtrl.getOneReply);
 //modification d'un produit
-router.put("/:idObject", [authJwt.verifyToken], upload.single('image'), replyCtrl.modifyReply);
+router.put("/:idObject", [authJwt.verifyToken], upload, replyCtrl.modifyReply);
 //suppression d'un produit
 router.delete("/:idObject", [authJwt.verifyToken], replyCtrl.deleteReply);
 
