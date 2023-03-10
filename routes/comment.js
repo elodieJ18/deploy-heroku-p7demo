990a6677306ce71b7522d8a0d95497f6c8b3b0f7
lody <lody@mbp-de-lody.home>
2023-03-10 14:59:59 -0800

    install-multer

--- routes/comment.js
+++ routes/comment.js
@@ -2,10 +2,13 @@ const express = require("express");
 const router = express.Router();
 const commentCtrl = require("../controllers/comment");
 const { authJwt } = require("../middleware");
-const multer = require("../middleware/multer");
+const parser = require("../middleware/multer");
 
 //creation d'une nouvelle sauce
-router.post("/upload", [authJwt.verifyToken], multer, commentCtrl.createComment);
+//router.post("/upload", [authJwt.verifyToken], multer, commentCtrl.createComment);
+router.post('/upload', [authJwt.verifyToken], parser.single('image'), commentCtrl.createComment, function (req, res) {
+    res.json(req.file);
+});
 //afficher toutes les sauces
 router.get("/",[authJwt.verifyToken], commentCtrl.getallComment);
 //affichage du produit dans sa propre page
